9c54dc2b6d9cb376bb711010d4707187916edbd7
DEMENT0R <dmntr@rambler.ru>
2018-11-02 15:46:21 +1000

    test panel zone

--- _index.php
+++
@@ -1,1 +0,0 @@
-<?php include 'cube/index.html'; ?>
--- app.php
+++ app.php
@@ -5,14 +5,8 @@ $sess_id = "";
 $user_id = "";
 $user_name = "";
 
-//db config
-include 'config/config.php';
-
 //Show misc info
 include 'partials/echo.php';
-
-//Authorisation
-include 'partials/auth.php';
 
 //Sending text
 if ($_POST['text'] & $isAuth){
