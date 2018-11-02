7bdc908c30ae52d26c2347ff03d8f0b8f519af26
DEMENT0R <dmntr@rambler.ru>
2018-11-02 15:48:14 +1000

    test panel zone

---
+++ panel/crud.php
@@ -0,0 +1,59 @@
+<?php 
+    //PHP counter starts:
+    $_start_time = microtime(true); 
+?>
+<?php
+include '../classes/safemysql.class.php';
+
+$db     = new SafeMysql();
+$table  = "users";
+$fields = ['name', 'ssid', 'password', 'mail', 'text', 'sex'];
+
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+    $data = $db->filterArray($_POST, $fields);
+    if (isset($_POST['delete']))
+    {
+        $db->query("DELETE FROM ?n WHERE id=?i", $table, $_POST['delete']);
+    } elseif ($_POST['id']) {
+        $db->query("UPDATE ?n SET ?u WHERE id = ?i", $table, $data, $_POST['id']);
+    } else {
+        $db->query("INSERT INTO ?n SET ?u", $table, $data);
+    }
+    header("Location: http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']);
+    exit;
+}
+
+if (!isset($_GET['id']))
+{
+    $LIST = $db->getAll("SELECT * FROM ?n", $table);
+    include 'list.php';
+} else {
+    if ($_GET['id'])
+    {
+        $row = $db->getRow("SELECT * FROM ?n WHERE id=?i", $table, $_GET['id']);
+    } else {
+        $row['name'] = '';
+        $row['ssid'] = '';
+        $row['password'] = '';
+        $row['mail'] = '';
+        $row['sex']  = '';
+        $row['text']  = '';
+        $row['id']   = 0;
+    }
+    include 'form.php';
+}
+
+// эта функция не нужна для работы с БД, но нужна для примеров вывода
+// поскольку вывод всегда должен быть экранирован
+function e($str)
+{
+   return htmlspecialchars($str, ENT_QUOTES, 'utf-8');
+}
+
+?>
+
+<?php
+    //PHP counter ends:
+    echo '<br><p class="text-center">Сгенерировано за '.(microtime(true) - $_start_time).' сек.</p>';
+?>
---
+++ panel/list.php
@@ -0,0 +1,47 @@
+<div class="container">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <br>
+    <div class="btn-group" role="group" aria-label="...">
+        <a href="/" class="btn btn-default">Главная</a>
+        <a href="?id=0" class="btn btn-default">Добавить</a>
+        <a href="/" onclick="javascript:void(document.cookie = 'ssid=')" class="btn btn-default">Лог аут</a>
+    </div>
+    <hr>
+    <table class="table">
+        <tr>
+            <td><b>id</b></td>
+            <td><b>Имя</b></td>
+            <td><b>ssid</b></td>
+            <td><b>Пароль</b></td>
+            <td><b>Мыло</b></td>
+            <td><b>Текст</b></td>
+            <td><b>Пол</b></td>
+            <td><b>Обновлено</b></td>
+            <td><b>Действия</b></td>
+        </tr>
+    <?php foreach ($LIST as $row): ?>
+        <tr>
+            <td><?=e($row['id'])?></td>
+            <td><?=e($row['name'])?></td>
+            <td><?=e($row['ssid'])?></td>
+            <td><?=e($row['password'])?></td>
+            <td><?=e($row['mail'])?></td>
+            <td><?=e($row['text'])?></td>
+            <td><?=e($row['sex'])?></td>
+            <td><?=e($row['updated'])?></td>
+            <td>
+
+                <form method="POST">
+                    <input type="hidden" name="delete" value="<?=e($row['id'])?>">
+                    <div class="btn-group" role="group" aria-label="...">
+                    <a href="/" onclick="javascript:void(document.cookie = 'ssid=<?=e($row['ssid'])?>')" class="btn btn-success">Войти</a>
+                    <a href="?id=<?=e($row['id'])?>" class="btn btn-warning">Правка</a>
+                    <input type="submit" value="Удалить!" class="btn btn-danger">
+                    </div>
+                </form>
+            </td>
+        </tr>
+    <?php endforeach ?>
+    </table>
+</div>
