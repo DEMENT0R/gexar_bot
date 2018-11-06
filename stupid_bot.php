a2c2c51ccb55344988b6198d0399c4fe03e9ffe1
DEMENT0R <dmntr@rambler.ru>
2018-11-06 17:46:20 +1000

    Stupid Bot Added

--- stupid_bot.php
+++ stupid_bot.php
@@ -1,57 +1,57 @@
 <?php
 
-for ($i = 0; $i < 10; $i++){
-	echo $i; 
-	sleep (1);
-}
+for ($i = 0; $i < 60; $i++){
+	//echo $i; 
 
-$ssid = "C1k5fbviTa";
+	$ssid = "C1k5fbviTa";
 
-include 'classes/safemysql.class.php';
-$db     = new SafeMysql();
-$table  = "chats";
-$fields = ['id', 'ssid', 'name', 'text', 'updated'];
+	include 'classes/safemysql.class.php';
+	$db     = new SafeMysql();
+	$table  = "chats";
+	$fields = ['id', 'ssid', 'name', 'text', 'updated'];
 
-$row = $db->getRow("SELECT * FROM ?n WHERE ssid=?s ORDER BY updated DESC LIMIT 1", $table, $ssid);
+	$row = $db->getRow("SELECT * FROM ?n WHERE ssid=?s ORDER BY updated DESC LIMIT 1", $table, $ssid);
 
-$username = $row['name'];
-$text = $row['text'];
-if ($username == 'StupidBot') {
-	exit("Ответ уже написан ботом!");
-}
+	$username = $row['name'];
+	$text = $row['text'];
+	if ($username == 'StupidBot') {
+		exit("Ответ уже написан ботом!");
+	}
 
-echo "text = ".$text;
+	echo "text = ".$text;
 
-if ($text == 'Привет') {
-	$text = "Приветствую!";
-	echo "Приветствие обнаружено! Отвечаю!";
-	sendingMessage($text);
-} else if ($text == 'Пока'){
-	$text = "До связи!!";
-	sendingMessage($text);
-} else if ($text == 'Кто ты'){
-	$text = "Меня зовут Tomoru";
-	sendingMessage($text);
-} else if ($text == 'Анекдот'){
-	$text = "Из письма в газету: `У меня конфисковали самогонный аппарат. Могу ли я получить компенсацию в связи с потерей кормильца? `";
-	sendingMessage($text);
-}
+	if ($text == 'Привет') {
+		$text = "Приветствую!";
+		echo "Приветствие обнаружено! Отвечаю!";
+		sendingMessage($text);
+	} else if ($text == 'Пока'){
+		$text = "До связи!!";
+		sendingMessage($text);
+	} else if ($text == 'Кто ты'){
+		$text = "Меня зовут Tomoru";
+		sendingMessage($text);
+	} else if ($text == 'Анекдот'){
+		$text = "Из письма в газету: `У меня конфисковали самогонный аппарат. Могу ли я получить компенсацию в связи с потерей кормильца? `";
+		sendingMessage($text);
+	}
 
-if ($text){
-	$text = "Не понимаю... Мне известны тольско следующие слова: Привет, Пока, Кто ты, Анекдот";
-	sendingMessage($text);
-}
+	if ($text){
+		$text = "Не понимаю... Мне известны тольско следующие слова: Привет, Пока, Кто ты, Анекдот";
+		sendingMessage($text);
+	}
 
 //functions
 //sending message
-function sendingMessage($text){
-	global $db, $table, $ssid;
-	$db->query("INSERT INTO ".$table." SET ssid='".$ssid."', name='Stupid Bot', text='".$text."'");
-	exit("Конец!");
-}
+	function sendingMessage($text){
+		global $db, $table, $ssid;
+		$db->query("INSERT INTO ".$table." SET ssid='".$ssid."', name='Stupid Bot', text='".$text."'");
+		exit("Конец!");
+	}
 
-function test(){
+	function test(){
 	//test
-}
+	}
 
+	sleep (1);
+}
 ?>
