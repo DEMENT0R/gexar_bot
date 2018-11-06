36e9d8f9d7d0fb5d3202022d8c974a9e6da6d0fe
DEMENT0R <dmntr@rambler.ru>
2018-11-06 16:18:31 +1000

    Stupid Bot Added

--- stupid_bot.php
+++ stupid_bot.php
@@ -19,10 +19,10 @@ if ($username == 'StupidBot') {
 	exit("Ответ уже написан ботом!");
 }
 
-if (((strpos($text, 'Привет')) or (strpos($text, 'Здравствуй')) or (strpos($text, 'Хай') or (strpos($text, 'Привет')) or (strpos($text, 'Здравствуй')) or (strpos($text, 'Хай'))) {
+if ((strpos($text, 'Привет')) or (strpos($text, 'Здравствуй')) or (strpos($text, 'Хай')) or (strpos($text, 'привет')) or (strpos($text, 'здравствуй')) or (strpos($text, 'хай'))) {
 	$text = "Приветствую!"
-	//sendingMessage($text);
-	$db->query("INSERT INTO `".$table."` SET ssid='".$ssid."'");
+	sendingMessage($text);
+	//$db->query("INSERT INTO `".$table."` SET ssid='".$ssid."'");
 	//$db->query("INSERT INTO ?n SET ssid='".$ssid."' name='Stupid Bot' text='".$text."'", $table);
 }
 
