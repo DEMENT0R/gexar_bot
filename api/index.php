4a0d5099f9537ec7087d25973240b07fa94f65f7
DEMENT0R <dmntr@rambler.ru>
2018-11-09 11:38:09 +1000

    Update

--- api/index.php
+++ api/index.php
@@ -41,6 +41,8 @@ if($_SERVER['REQUEST_METHOD']=='POST')
 <?php
 //PHP counter ends:
 $output[1] =  round((microtime(true) - $_start_time), 5).' sec.';
+
+var_dump($_POST);
 echo json_encode($output);
 
 ?>
