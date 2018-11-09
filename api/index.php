1cf1aeb7b568c2ae054b6d02b6320cd9034f2746
DEMENT0R <dmntr@rambler.ru>
2018-11-09 11:33:14 +1000

    Update

---
+++ api/index.php
@@ -0,0 +1,46 @@
+<?php
+//PHP counter starts:
+$_start_time = microtime(true);
+?>
+<?php
+
+$output = ['', ''];
+
+$ssid = "";
+
+include '../classes/safemysql.class.php';
+$db     = new SafeMysql();
+$table  = "chats";
+$fields = ['id', 'ssid', 'name', 'text', 'updated'];
+
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+	$data = $db->filterArray($_POST, $fields);
+	if (isset($_POST['get_message'])) {
+		////////////////////
+		//GETting messages//
+		////////////////////
+		$db->query("SELECT FROM ?n WHERE ssid=?s ORDER BY updated DESC LIMIT 1", $table, $_POST['ssid']);
+		$output[0] = "message got";
+	} elseif ($_POST['send_message']) {
+		////////////////////
+		//SENDing messages//
+		////////////////////
+		$db->query("INSERT INTO ?n SET ?u", $table, $data);
+		//echo 'SEND MESSAGE = '.$_POST;
+		$output[0] =  "message sended";
+	}
+	header("Location: http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']);
+	exit;
+} else {
+	//echo "<h1>Not POST method!!!</h1>";
+	$output[0] = "NO POST!";
+}
+
+?>
+<?php
+//PHP counter ends:
+$output[1] =  '<!-- generate time: '.round((microtime(true) - $_start_time), 5).' sec.';
+echo json_encode($output);
+
+?>
