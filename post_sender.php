e83b7cfeee41ea296649eeba72cf0b9579119995
DEMENTOR <dmntr@rambler.ru>
2018-11-10 00:04:50 +1000

    Update post_sender.php

--- post_sender.php
+++ post_sender.php
@@ -10,6 +10,7 @@
 <?php
 */
 
+
 $ssid = $_COOKIE['ssid'];
 if ($ssid == '') {
   $ssid = "0000";
@@ -25,6 +26,8 @@ if (!$_GET['url']){
   $url = $_GET['url'];
 }
 
+echo "url = ".$url."<hr>";
+
 $params = array(
   'sessionid' => $ssid,
   'message' => $text
@@ -32,11 +35,35 @@ $params = array(
 $result = file_get_contents($url, false, stream_context_create(array(
   'http' => array(
     'method'  => 'POST',
-    'header'  => 'Content-type: application/json',
+    'header'  => 'Content-Type: application/json',
     'content' => http_build_query($params)
   )
 )));
 
 echo $result;
 
+
+?>
+
+<?php
+/*
+$url = $_GET['url'];
+
+$data='{"sessionid": "000","message": "Hello!"}';
+$json=json_encode($data);
+
+$ch = curl_init($url);
+
+curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
+curl_setopt($ch,CURLOPT_PROTOCOLS,CURLPROTO_HTTPS);
+curl_setopt($ch,CURLOPT_HTTP_VERSION,CURL_HTTP_VERSION_1_1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST,'POST');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Basic lock', 'Accept: application/json', 'Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo json_decode($result);
+*/
 ?>
