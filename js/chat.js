fa2aaff35c282eef2a59669d1a3ef920d42dcba5
DEMENTOR <dmntr@rambler.ru>
2018-11-12 13:33:32 +1000

    Changing: 4 to 3 rows

--- js/chat.js
+++ js/chat.js
@@ -27,7 +27,7 @@ $("#chat-input-field").keyup(function(event){
 
 //update function
 function UpdateMessages(){
-  $('#chat-history').load('app.php?get_message=4');
+  $('#chat-history').load('app.php?get_message=3');
   $.post("stupid_bot.php");
 }
 
